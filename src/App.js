f38b25ab00b620fc119406ba637de16dac3baa37
amgad behman <amgadbehman12345@gmail.com>
2019-11-20 21:59:44 -0800

    part like its 1987

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { useState, useEffect } from 'react'
 
-class App extends Component {
-  render() {
-    return <HelloWorld />
+// import axios from 'axios'
+import Moveis from './components/Moveis'
+
+const App = () => {
+  const [moveis, setMoveis] = useState([])
+
+  const getDataFromAPI = async () => {
+    const resp = await fetch(
+      'https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=0bc0728f34b625ce3a0ce16d93e6973e'
+    )
+    const data = await resp.json()
+    console.log(data)
+    setMoveis(data.results)
   }
+
+  useEffect(() => {
+    console.log('using the effect')
+    // call my API
+    getDataFromAPI()
+  }, [])
+
+  return (
+    <>
+     <h1> Party Like Its 1989 </h1>
+      <ul>
+       
+
+        {moveis.map(Movie => {
+          return (
+            <Moveis
+              key={Movie.id}
+              title={Movie.title}
+              overview={Movie.overview}
+              image={Movie.poster_path}
+              
+            />
+          )
+        })}
+      </ul>
+    </>
+  )
 }
 
 export default App
